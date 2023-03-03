cd311df2ba35613b0a3a94bcde7e48434bc44745
adem <a.ayar94@gmail.com>
2023-03-03 21:43:57 +0300

    Project navigation created,Fragments added

--- app/src/main/java/com/aayar94/earthquakes/MainActivity.kt
+++ app/src/main/java/com/aayar94/earthquakes/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.aayar94.earthquakes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.aayar94.earthquakes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostController) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfig = AppBarConfiguration(
+            setOf(
+                R.id.lastEarthquakesFragment,
+                R.id.riskMapFragment
+            )
+        )
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
     }
 }
