9a0f01cc28e69a3fc8251175fa103daa5aaa8456
adem <a.ayar94@gmail.com>
2023-03-03 21:21:12 +0300

    init project, gradle dependencies added

---
+++ app/src/main/java/com/aayar94/earthquakes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aayar94.earthquakes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
